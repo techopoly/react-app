d8f9b8bfda984038e382dba465d2599c635e4d9a
Ishmam <techopoly444@gmail.com>
2020-12-13 23:26:37 +0600

    7(17)

--- chapter-3/trying-out-basics/src/components/cockpit/cockpit.js
+++ chapter-3/trying-out-basics/src/components/cockpit/cockpit.js
@@ -5,25 +5,28 @@ import './cockpit.css';
 
 const Cockpit = (props)=>{
     const {showPerson, btnHandler, title} = props
-    
-    useEffect(()=>{
+/*    
+     useEffect(()=>{
         console.log('[cockpit.js] useEffect');
 
     }, [showPerson]);/*  the second arg is for defining for which props change useEffect will trigger.
     if you want to trigger the useEffect only the first time this component is created then pass an empty array */
-
-     
-    useEffect(()=>{
-        console.log('[cockpit.js] useEffect--empty array');
-    }, []);
-
-
+ 
     useEffect(()=>{
         console.log('[cockpit.js] useEffect with return function')
         return ()=>{
             console.log('[cockpit.js] cleaning up before unmounting') //this will run when you unmount cockpit.js
         }
     })
+    
+
+ 
+/* 
+    useEffect(()=>{
+        console.log('[cockpit.js] useEffect--empty array');
+    }, []);
+
+ */
     
     
     let buttonColor = ''
@@ -33,6 +36,7 @@ const Cockpit = (props)=>{
     else{
         buttonColor = 'buttonYellow'
     }
+    console.log('still running')
     return (
         <div className='cockpit'>
           <h1>{title}</h1>
@@ -41,4 +45,4 @@ const Cockpit = (props)=>{
     )
 }
 
-export default Cockpit;
+export default React.memo(Cockpit); //alternative to shouldComponentUpdate(){} for funcitonal components
