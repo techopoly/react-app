6d9898db58729021ae7ffc34dd69ccfd0acac36a
Ishmam <techopoly444@gmail.com>
2020-12-14 18:52:35 +0600

    upto 24

--- chapter-3/trying-out-basics/src/components/persons/person/Person.js
+++ chapter-3/trying-out-basics/src/components/persons/person/Person.js
@@ -1,16 +1,13 @@
-/* eslint-disable no-restricted-globals */
-/* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
 import './person.css';
 import Aux from '../../../hoc/auxilliary.js';
+import withClass from '../../../hoc/withClass.js'
 class Person extends Component {
-
-
     render() {
         console.log('[Person.js] rendering...')
         return (
             <Aux>
-                <div className='person'>
+                <div >
                     <h3>My name is {this.props.name}</h3>
                     <p>My age is {this.props.age}</p>
                     <input onChange={(event) => { this.props.changed(event, this.props.id) }}></input>
@@ -18,11 +15,8 @@ class Person extends Component {
                 <h1>this is h1</h1>
                 {this.props.children}
             </Aux>
-
         )
     }
-
-
 }
 
-export default Person;
+export default withClass(Person, 'person');
